92a8fb669e56cb149e6ad1c91fb77840dbf0098e
rhyunyp <rhyun@usc.edu>
2016-10-25 16:30:51 -0700

    Add files via upload

---
+++ HW #3 Problem 7 and 8.R
@@ -0,0 +1,102 @@
+######################################
+# 12.10.2016
+# Logistic regression on ChIP-seq data
+# BISC 481
+######################################
+
+## Install packages
+install.packages("caret")
+install.packages("e1071")
+install.packages("ROCR")
+install.packages("Biostrings")
+library(Biostrings)
+
+## Initialization
+library(DNAshapeR)
+library(caret)
+library(ROCR)
+workingPath <- "/Users/roberthyun/Desktop/BISC481-master/CTCF/"
+
+## Generate data for the classifcation (assign Y to bound and N to non-bound)
+# bound
+boundFasta <- readDNAStringSet(paste0(workingPath, "bound_500.fa"))
+sequences <- paste(boundFasta)
+boundTxt <- data.frame(seq=sequences, isBound="Y")
+
+# non-bound
+nonboundFasta <- readDNAStringSet(paste0(workingPath, "unbound_500.fa"))
+sequences <- paste(nonboundFasta)
+nonboundTxt <- data.frame(seq=sequences, isBound="N")
+
+# merge two datasets
+writeXStringSet( c(boundFasta, nonboundFasta), paste0(workingPath, "ctcf.fa"))
+exp_data <- rbind(boundTxt, nonboundTxt)
+
+
+## DNAshapeR prediction
+pred <- getShape(paste0(workingPath, "ctcf.fa"))
+plotShape(pred$Roll)
+
+## DNAshapeR prediction
+pred <- getShape(paste0(workingPath, "ctcf.fa"))
+plotShape(pred$MGW)
+
+## DNAshapeR prediction
+pred <- getShape(paste0(workingPath, "ctcf.fa"))
+plotShape(pred$ProT)
+
+## DNAshapeR prediction
+pred <- getShape(paste0(workingPath, "ctcf.fa"))
+plotShape(pred$HelT)
+
+
+
+
+
+
+## Generate data for the classifcation (assign Y to bound and N to non-bound)
+# bound
+boundFasta <- readDNAStringSet(paste0(workingPath, "bound_30.fa"))
+sequences <- paste(boundFasta)
+boundTxt <- data.frame(seq=sequences, isBound="Y")
+
+# non-bound
+nonboundFasta <- readDNAStringSet(paste0(workingPath, "unbound_30.fa"))
+sequences <- paste(nonboundFasta)
+nonboundTxt <- data.frame(seq=sequences, isBound="N")
+
+# merge two datasets
+writeXStringSet( c(boundFasta, nonboundFasta), paste0(workingPath, "ctcf.fa"))
+exp_data <- rbind(boundTxt, nonboundTxt)
+
+## Encode feature vectors 1-mer
+featureType <- c("1-mer")
+featureVector <- encodeSeqShape(paste0(workingPath, "ctcf.fa"), pred, featureType)
+df <- data.frame(isBound = exp_data$isBound, featureVector)
+
+
+## Encode feature vectors 1-mer 1-shape
+featureType <- c("1-mer", "1-shape")
+featureVector <- encodeSeqShape(paste0(workingPath, "ctcf.fa"), pred, featureType)
+df <- data.frame(isBound = exp_data$isBound, featureVector)
+
+## Logistic regression
+# Set parameters for Caret
+trainControl <- trainControl(method = "cv", number = 10, 
+                             savePredictions = TRUE, classProbs = TRUE)
+# Perform prediction
+model <- train(isBound~ ., data = df, trControl = trainControl,
+               method = "glm", family = binomial, metric ="ROC")
+summary(model)
+
+## Plot AUROC
+prediction <- prediction( model$pred$Y, model$pred$obs )
+performance <- performance( prediction, "tpr", "fpr" )
+plot(performance)
+
+## Caluculate AUROC
+auc <- performance(prediction, "auc")
+auc <- unlist(slot(auc, "y.values"))
+auc
+
+
